9a4ea860507f2faa688ea0fe576ba000aa6be872
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2026-06-10 21:52:02 -0400

    final comm

--- src/test/java/com/orangeHRM/generic/AdminBaseControl.java
+++ src/test/java/com/orangeHRM/generic/AdminBaseControl.java
@@ -95,11 +95,7 @@ public abstract class AdminBaseControl  implements Autoconstant  {
         if (browsername.equalsIgnoreCase("chrome")) {
 
            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--window-size=1920,1080");
+           
             
 
             driver = new ChromeDriver( options);
